83f8837979b01842780238db4935122ad4831206
DLM-01-code <jesteipooldaniel@gmail.com>
2025-12-17 15:40:23 +0100

    Refresh_function_"def"

--- casino.py
+++ casino.py
@@ -2,9 +2,9 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     if min < 1 or max > 1000:
-        return []
+        return 
     if quantity > (max - min + 1):
-        return []
+        return 
     return sorted(random.sample(range(min, max + 1), quantity))
 
 min = int(input("min: "))
