42379d224da58af6527a9e3467dea90d2b08255d
DLM-01-code <jesteipooldaniel@gmail.com>
2026-01-17 15:50:53 +0100

    1

--- datetime(static_date).py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def get_days_from_today(date):
-    today = datetime.now()
-    return (today - date).days
-
-
-date = datetime(2020, 10, 9).center(100)
-print(f"-{get_days_from_today(date)}")
